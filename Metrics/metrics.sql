0152b76aa3699c7c46f877dee82bc57bab327bc6
jooper <282376402@163.com>
2020-12-01 17:53:41 +0800

    添加会诊实时统计语句

--- Metrics/metrics.sql
+++ Metrics/metrics.sql
@@ -46,7 +46,7 @@ DISCOUNT_AFTER_AMT DECIMAL
     type ='kafka10',
     bootstrapServers ='master:9092,slave2:9092,slave3:9092',
     zookeeperQuorum ='master:2181,slave2:2181,slave3:2181/kafka',
-    offsetReset ='latest',
+    offsetReset ='earliest',
     topic ='opr_registration_d',
     timezone='Asia/Shanghai',
     topicIsPattern ='false',
@@ -58,7 +58,7 @@ CREATE TABLE opc_drug_presc_d_charge(
     type ='kafka10',
     bootstrapServers ='master:9092,slave2:9092,slave3:9092',
     zookeeperQuorum ='master:2181,slave2:2181,slave3:2181/kafka',
-    offsetReset ='latest',
+    offsetReset ='earliest',
     topic ='opc_drug_presc_d_charge',
     timezone='Asia/Shanghai',
     topicIsPattern ='false',
@@ -71,7 +71,7 @@ CREATE TABLE opc_drug_presc_d_charge(
     type ='kafka10',
     bootstrapServers ='master:9092,slave2:9092,slave3:9092',
     zookeeperQuorum ='master:2181,slave2:2181,slave3:2181/kafka',
-    offsetReset ='latest',
+    offsetReset ='earliest',
     topic ='opc_diag_service_d_charge',
     timezone='Asia/Shanghai',
     topicIsPattern ='false',
@@ -86,7 +86,7 @@ CREATE TABLE opc_drug_presc_d_charge(
  )WITH(
     type='kafka10',
     bootstrapServers='master:9092,slave2:9092,slave3:9092',
-    offsetReset ='latest',
+    offsetReset ='earliest',
     topic ='opc_registration',
     timezone='Asia/Shanghai',
     topicIsPattern ='false',
@@ -148,7 +148,7 @@ CREATE TABLE ipc_diag_service_d (
  )WITH(
     type='kafka10',
     bootstrapServers='master:9092,slave2:9092,slave3:9092',
-    offsetReset ='latest',
+    offsetReset ='earliest',
     topic ='ipi_registration',
     timezone='Asia/Shanghai',
     topicIsPattern ='false',
@@ -158,11 +158,11 @@ CREATE TABLE ipc_diag_service_d (
 
  CREATE TABLE ipd_consult_apply(
     ID VARCHAR ,
-    REQ_DATE VARCHAR
+    REQ_DATE TIMESTAMP
  )WITH(
     type='kafka10',
     bootstrapServers='master:9092,slave2:9092,slave3:9092',
-    offsetReset ='latest',
+    offsetReset ='earliest',
     topic ='ipd_consult_apply',
     timezone='Asia/Shanghai',
     topicIsPattern ='false',
@@ -173,98 +173,98 @@ CREATE TABLE ipc_diag_service_d (
 
 
 ---------------------------------------------------------------------------------------
-
---门诊收入
-insert into metric_sink_kfk
-select
-'1' as PRO_NAME,
-'opc_fee'as CODE,
-''as DT,
-'门诊收入'as LABEL,
-cast(sum(total_cost) as string) as DATA,
-'10' as REMARK from
-(select DISCOUNT_AFTER_AMT as total_cost from opr_registration_d
-union all
-select TOTAL_AMT as total_cost from opc_diag_service_d_charge
-union all
-select TOTAL_AMT as total_cost from opc_drug_presc_d_charge
-);
-
-
--- 门诊人次
-insert into metric_sink_kfk
-select
-'2' as PRO_NAME,
-'opc_diag_count'as CODE,
-''as DT,
-'门诊人次'as LABEL,
-cast(COUNT(DISTINCT PERSON_INFO_ID)as string) as DATA,
-'6' as REMARK
-from opc_registration;
-
-
-
-
---手术人数
-insert into metric_sink_kfk
-select
-'1' as PRO_NAME,
-'operation_count'as CODE,
-'手术例数'as LABEL,
-cast(count(DISTINCT ID) as string) as DATA,
-DATE_FORMAT(PROCTIME,'yyyy-mm-dd') as DT,
-'20' REMARK
-FROM mrm_first_page_operation
-group by DATE_FORMAT(PROCTIME,'yyyy-mm-dd');
-
-
-
-
-
---住院收入
-insert into metric_sink_kfk
-select
-'1' as PRO_NAME,
-'ipi_fee'as CODE,
-'住院收入'as LABEL,
-cast(sum(t.cnt) as string) as DATA,
-t.dt as DT,
-'5' REMARK
-from
-(
-select sum(TOTAL_AMT)cnt, DATE_FORMAT(PROCTIME,'yyyy-mm-dd') dt from ipc_drug_presc_d
-group by DATE_FORMAT(PROCTIME,'yyyy-mm-dd')
-union all
-select sum(TOTAL_AMT)cnt, DATE_FORMAT(PROCTIME,'yyyy-mm-dd') dt from ipc_diag_service_d
-group by DATE_FORMAT(PROCTIME,'yyyy-mm-dd')
-)t
-group by dt;
-
-
-
--- 住院人次
-insert into metric_sink_kfk
-select
-'2' as PRO_NAME,
-'ipi_count'as CODE,
-''as DT,
-'住院人次'as LABEL,
-cast(COUNT(DISTINCT PERSON_INFO_ID)as string) as DATA,
-'6' as REMARK
-from ipi_registration;
+--
+-- --门诊收入
+-- insert into metric_sink_kfk
+-- select
+-- 'opc_ipc' as PRO_NAME,
+-- 'opc_fee'as CODE,
+-- ''as DT,
+-- '门诊收入'as LABEL,
+-- cast(sum(total_cost) as string) as DATA,
+-- '10' as REMARK from
+-- (select DISCOUNT_AFTER_AMT as total_cost from opr_registration_d
+-- union all
+-- select TOTAL_AMT as total_cost from opc_diag_service_d_charge
+-- union all
+-- select TOTAL_AMT as total_cost from opc_drug_presc_d_charge
+-- );
+--
+--
+-- -- 门诊人次
+-- insert into metric_sink_kfk
+-- select
+-- 'opc_ipc' as PRO_NAME,
+-- 'opc_diag_count'as CODE,
+-- ''as DT,
+-- '门诊人次'as LABEL,
+-- cast(COUNT(DISTINCT PERSON_INFO_ID)as string) as DATA,
+-- '6' as REMARK
+-- from opc_registration;
+--
+--
+--
+--
+-- --手术人数
+-- insert into metric_sink_kfk
+-- select
+-- 'opc_ipc' as PRO_NAME,
+-- 'operation_count'as CODE,
+-- '手术例数'as LABEL,
+-- cast(count(DISTINCT ID) as string) as DATA,
+-- DATE_FORMAT(PROCTIME,'yyyy-mm-dd') as DT,
+-- '20' REMARK
+-- FROM mrm_first_page_operation
+-- group by DATE_FORMAT(PROCTIME,'yyyy-mm-dd');
+--
+--
+--
+--
+--
+-- --住院收入
+-- insert into metric_sink_kfk
+-- select
+-- 'opc_ipc' as PRO_NAME,
+-- 'ipi_fee'as CODE,
+-- '住院收入'as LABEL,
+-- cast(sum(t.cnt) as string) as DATA,
+-- t.dt as DT,
+-- '5' REMARK
+-- from
+-- (
+-- select sum(TOTAL_AMT)cnt, DATE_FORMAT(PROCTIME,'yyyy-mm-dd') dt from ipc_drug_presc_d
+-- group by DATE_FORMAT(PROCTIME,'yyyy-mm-dd')
+-- union all
+-- select sum(TOTAL_AMT)cnt, DATE_FORMAT(PROCTIME,'yyyy-mm-dd') dt from ipc_diag_service_d
+-- group by DATE_FORMAT(PROCTIME,'yyyy-mm-dd')
+-- )t
+-- group by dt;
+--
+--
+--
+-- -- 住院人次
+-- insert into metric_sink_kfk
+-- select
+-- 'opc_ipc' as PRO_NAME,
+-- 'ipi_count'as CODE,
+-- ''as DT,
+-- '住院人次'as LABEL,
+-- cast(COUNT(DISTINCT PERSON_INFO_ID)as string) as DATA,
+-- '6' as REMARK
+-- from ipi_registration;
 
 
 
 
 -- 会诊
--- insert into metric_sink_kfk
--- select
--- '2' as PRO_NAME,
--- 'ipi_count'as CODE,
--- to_char(req_date, 'hh24') || ':00' as DT,
--- '会诊'as LABEL,
--- cast(COUNT(DISTINCT ID)as string) as DATA,
--- '6' as REMARK
--- from ipd_consult_apply
--- group by to_char(req_date,'hh24')|| ':00';
+insert into metric_sink_kfk
+select
+'hz' as PRO_NAME,
+'hz_count'as CODE,
+concat_ws(':',cast(HOUR(REQ_DATE) as string),'00') as DT,
+'会诊'as LABEL,
+cast(COUNT(DISTINCT ID)as string) as DATA,
+'6' as REMARK
+from ipd_consult_apply
+group by concat_ws(':',cast(HOUR(REQ_DATE) as string),'00');
 
