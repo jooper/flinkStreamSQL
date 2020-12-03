41ea415fe7aeb5b678956e394483f69aaad8029f
jooper <282376402@163.com>
2020-12-03 15:41:46 +0800

    添加提交到集群的脚本文件

--- bin/startJob.sh
+++ bin/startJob.sh
@@ -1,2 +1,39 @@
 #!/usr/bin/env bash
-sh -x submit.sh -mode local -sql /home/flink/jobs/${1}.sql  -name $2  -localSqlPluginPath /home/flink/sqlplugins   -mode standalone -flinkconf /home/flink/flink-1.10.2/conf  -remoteSqlPluginPath /home/flink/sqlplugins
+#
+#sh -x submit.sh \
+#-mode local \
+#-sql /home/flink/jobs/${1} \
+#-name $2 \
+#-localSqlPluginPath /home/flink/sqlplugins \
+#-mode standalone \
+#-flinkconf /home/flink/flink-1.10.2/conf \
+#-remoteSqlPluginPath /flinkStreamPlugins
+#
+
+
+
+
+#sh submit.sh \
+#  -mode standalone \
+#  -sql /home/flink/jobs/${1} \
+#  -name $2 \
+#  -localSqlPluginPath /home/flink/sqlplugins \
+#  -remoteSqlPluginPath /flinkStreamPlugins \
+#  -flinkconf /home/flink/flink-1.10.2/conf \
+#  -flinkJarPath /home/flink/flink-1.10.2/lib \
+#  -pluginLoadMode shipfile
+
+
+
+#--ok
+#--use case
+#--./startJob.sh xxxx.sql  metric_name
+
+sh submit.sh \
+  -mode standalone \
+  -sql /home/flink/jobs/${1} \
+  -name $2 \
+  -localSqlPluginPath /home/flink/sqlplugins \
+  -remoteSqlPluginPath /home/flink/sqlplugins \
+  -flinkconf /home/flink/flink-1.10.2/conf \
+  -flinkJarPath /home/flink/flink-1.10.2/lib \
